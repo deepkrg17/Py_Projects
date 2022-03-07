d00efe75bd0232f1fb784d16059cdd878ae7e163
deepkrg17 <75083264+deepkrg17@users.noreply.github.com>
2022-03-07 12:41:48 +0530

    Refactoring NGG

--- Python Games/Number Guessing Game.py
+++ Python Games/Number Guessing Game.py
@@ -3,20 +3,17 @@ a = random.randint(1, 100)
 i = 10
 while(i!=0):
 	print(f"You have {i} guesses")
-	b = int(input("Enter a number between 1 & 100:    "))
+	b = int(input("\nEnter a number between 1 & 100:    "))
 	if b>a:
 		print("\nLower Number please.")
-		i-= 1
-		continue
-	if b<a:
+		i -= 1
+	elif b<a:
 		print("\nHigher number please.")
-		i-= 1
-		continue
+		i -= 1
 	else:
 		print("\nCongratulations!🤩")
 		i-=1
+		print(f"You have guessed it in {10-i} guesses.")
 		break
-if i==0:
-	print(f"\nSorry. You have spent 10 guesses.☹️ The number is {a}.")
-else:
-	print(f"You have guessed it in {10-i} guesses.")
+	if i==0:
+		print(f"\nSorry. You have spent 10 guesses.☹️ The number is {a}.")
