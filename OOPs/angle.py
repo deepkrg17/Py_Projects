79495f858f99256860adfe57edabd116f15f4496
deepkrg17 <75083264+deepkrg17@users.noreply.github.com>
2022-07-06 11:22:42 +0530

    Adding Angle.py

---
+++ OOPs/angle.py
@@ -0,0 +1,28 @@
+class Angle():
+    def __init__(self, deg, mint, sec):
+        ext_mint, self.sec = divmod(sec, 60)
+        ext_deg, self.mint = divmod(mint+ext_mint, 60)
+        self.deg = deg + ext_deg
+
+    @staticmethod
+    def avg(*args):
+        deg = mint = sec = 0
+        n = len(args)
+        for ang in args:
+            deg += ang.deg
+            mint += ang.mint
+            sec += ang.sec
+
+        deg, ext_deg = divmod(deg, n)
+        mint, ext_mint = divmod(mint+(ext_deg*60), n)
+        sec = (sec+(ext_mint*60))//n
+
+        return Angle(deg, mint, sec)
+
+    def __add__(self, other):
+        return Angle(self.deg+other.deg,
+                    self.mint+other.mint,
+                    self.sec+other.sec)
+
+    def __str__(self):
+        return f"{self.deg}°{self.mint}'{self.sec}\""
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Python_Projects
-My Python projects.
+# Python Projects
+
+## Python CLI Games
+ - [Rock Paper Scissor](Python%20Games/Rock_Paper_Scissor.py)
+ - [Number Guessing Game](Python%20Games/Number%20Guessing%20Game.py)
+
+## Python OOPs
+ - [Geometry](OOPs/geometry.py) : Some Point and Line functions.
+ - [Angle](OOPs/angle.py) : Calculate averages of Angles with minutes and seconds.[^1] An addition method is also added.
+
+[^1]: In my physics projects, I had to calculate averages of 3 complex angles. When I couldn't find anything for it, I made this.
